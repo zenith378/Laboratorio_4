22a391b89ecce56f04411401cfc6f167105e7abe
Giulio Cordova <zenith378@MacBook-Pro-di-Giulio.local>
2022-12-01 18:10:35 +0100

    fit cal

--- Compton/prova.C
+++ Compton/prova.C
@@ -1,8 +1,20 @@
 #include "Spectrum.h"
-#include "fit2g.h"
+#include "fitg.h"
+#include "fitg_stronzio.h"
 
 int prova(){
     Spectrum();
-    fit2g("h",2900,4200);
+    fitg("h_cesio",2900,4200);
+    //c1->SaveAs("fit_Cesio.pdf");
+
+    //fitg("h_cobalto",2900,4200);
+    //c1->SaveAs("fit_Cobalto.pdf");
+
+    fitg("h_sodio",2400,3000);
+    //c1->SaveAs("fit_sodio.pdf");
+
+    fitg_stronzio("h_stronzio",100,3000);
+    //c1->SaveAs("fit_stronzio.pdf");
+
     return 0;
 }
